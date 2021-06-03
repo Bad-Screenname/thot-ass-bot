d2cdd873d6faaf78de20c37c6933d153fcb8dc24
Dominick Scianna <dominickscianna@gmail.com>
2021-06-02 22:47:18 -0400

    added help to command prefix reminder

--- thot-bot.py
+++ thot-bot.py
@@ -69,7 +69,7 @@ async def messages(ctx):
                     await ctx.channel.send('nice')
 
     #bandaid for new command prefix change UPDATE when you implement cog listeners
-    if 'hmb' or 'bully' or 'add' or 'delete' or 'list' or 'coin' or 'd20' in ctx.content.lower():
+    if 'hmb' or 'bully' or 'add' or 'delete' or 'list' or 'coin' or 'd20' or 'help' in ctx.content.lower():
         await ctx.channel.send('''just a friendly reminder that "." is the command prefix!
 if you need further help just use .help.''')
 
