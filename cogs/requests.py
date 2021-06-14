ad14b2959f3d53a4341b1d1d96f8edeeeb216241
Dominick Scianna <dominickscianna@gmail.com>
2021-06-14 12:50:26 -0400

    ctx doesnt have attribute content, ctx.message.content instead

--- cogs/requests.py
+++ cogs/requests.py
@@ -10,7 +10,7 @@ class Feature(commands.Cog):
     @commands.command(name='request', help='use this command to ask me to add a new feature')
     async def request(self, ctx):
         text_msg = self.msg_client.messages.create(
-            body = f'{ctx.author.name} asks "{ctx.content.split(".request")[1]}"',
+            body = f'{ctx.author.name} asks "{ctx.message.content.split(".request")[1]}"',
             from_ = os.getenv('twilio_number'),
             to = os.getenv('my_number')
         )
